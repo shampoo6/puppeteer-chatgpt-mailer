54b6ad8f3e6d98c0464d46390332c88c6ea733dc
shampoo6 <shampoo6@163.com>
2023-03-21 11:31:46 +0800

    update

--- src/workers/signInChat.ts
+++ src/workers/signInChat.ts
@@ -7,18 +7,28 @@ export default async function (page: Page): Promise<void> {
     const loginBtnSelector = '.btn.relative.btn-primary:nth-child(1)'
     await page.waitForSelector(loginBtnSelector)
     await wait(2000)
-    await page.click(loginBtnSelector)
+    await Promise.all([
+        page.waitForNavigation(),
+        page.click(loginBtnSelector)
+    ])
     const accountInputSelector = '#username'
     await page.waitForSelector(accountInputSelector)
     await page.type(accountInputSelector, chatConfig.email)
     await wait(2000)
-    await page.click('button[type="submit"]')
+    await Promise.all([
+        page.waitForNavigation(),
+        page.click('button[type="submit"]')
+    ])
     const pwdInputSelector = '#password'
     await page.waitForSelector(pwdInputSelector)
     await page.type(pwdInputSelector, chatConfig.password)
     await wait(2000)
-    await page.click('button[type="submit"]')
-    await page.waitForNavigation()
+    // await page.click('button[type="submit"]')
+    // await page.waitForNavigation()
+    await Promise.all([
+        page.waitForNavigation(),
+        page.click('button[type="submit"]')
+    ])
     await wait(1000)
     const headlessuiSelector = '#headlessui-portal-root'
     await page.waitForSelector(headlessuiSelector)
